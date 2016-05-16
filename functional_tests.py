dff6eb7b04b0fe2bcd0ba9704a8c1fb81cc5b4fb
Michael Bruce <codeamend@gmail.com>
2016-05-16 10:02:05 -0400

    refactor: row_text can now be searched for in a method

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Lacy hears about a To-Do site and visits the home page
@@ -30,23 +35,13 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # Lacy types in "buy eggs for egg salad"
-        inputbox.send_keys('Buy eggs for egg salad')
+        inputbox.send_keys('buy eggs for egg salad')
 
         # When she hits enter the page is updated with
         # "1: buy eggs for egg salad"
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        # self.assertTrue(
-        #     any(row.text == '1: buy eggs for egg salad' for row in rows),
-        #     "New to-do item did not appear in table -- its text was:\n%s" % (
-        #         table.text
-        #     )
-        # )
-        # Much simpler test
-        self.assertIn('1: Buy eggs for egg salad', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: buy eggs for egg salad')
 
         # There is still a text box waiting for another to-do item
         # she enters "make egg salad"
@@ -54,12 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("make egg salad")
 
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '2: make egg salad',
-            [row.text for row in rows]
-        )
+
+        self.check_for_row_in_list_table('2: make egg salad')
 
         # she wonders if the site will remember her list
         # she sees that a unique URL was generated for her -- some explanatory text as well
