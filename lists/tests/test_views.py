92c3c03535c8b383b57973942c981be8c98a87f0
Michael Bruce <codeamend@gmail.com>
2016-06-12 21:58:51 -0400

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -20,35 +20,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'The very next item'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        self.assertEqual(saved_items[0].text, 'The first (ever) list item')
-        self.assertEqual(saved_items[0].list, list_)
-        self.assertEqual(saved_items[1].text, 'The very next item')
-        self.assertEqual(saved_items[1].list, list_)
-
-
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
